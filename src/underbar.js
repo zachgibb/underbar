8dd1b8a4f396a35996c8875ec77f8213bceaa7b7
Zach Gibb <zachgibb@gmail.com>
2014-07-09 17:41:24 -0400

    Fixed _.filter

--- src/underbar.js
+++ src/underbar.js
@@ -80,6 +80,15 @@ var _ = {};
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var result = [];
+
+    _.each(collection, function(element) {
+      if ( test(element) === true ) {
+        result.push(element);
+      };
+    });
+
+    return result;
   };
 
   // Return all elements of an array that don't pass a truth test.

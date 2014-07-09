bd64a6562ac536346bcca9cc6de78d5018f9351e
Zach Gibb <zachgibb@gmail.com>
2014-07-09 17:24:14 -0400

    Fixed _.last function

--- src/underbar.js
+++ src/underbar.js
@@ -39,6 +39,9 @@ var _ = {};
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    return n === undefined ? array[array.length-1] : array.slice( 
+      n >= array.length ? 0 : array.length-n
+    );
   };
 
   // Call iterator(value, key, collection) for each element of collection.
